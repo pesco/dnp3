eace786a5df2c889784417a3014bacaad25a06e7
jadamcrain <jadamcrain@gmail.com>
2015-11-06 11:29:01 -0800

    placeholder project for plugin tests using catch

---
+++ test/plugin/main.cpp
@@ -0,0 +1,16 @@
+
+#define CATCH_CONFIG_RUNNER
+
+#include "../catch/catch.hpp"
+
+int main( int argc, char* const argv[] )
+{
+  // global setup...
+
+  int result = Catch::Session().run( argc, argv );
+
+  // global clean-up...
+
+  return result;
+}
+

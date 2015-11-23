7d883711ee48fa5eb668bdde058179f776cf2484
Sven M. Hallberg <pesco@khjk.org>
2015-11-23 19:50:54 +0100

    integrate filter and pprint back into one utility

--- examples/plugins/plugin_main.c
+++ examples/plugins/plugin_main.c
@@ -7,7 +7,34 @@
 #include <dnp3hammer/dnp3.h>
 
 
-/// callbacks ///
+/// helpers ///
+
+static void output(void *env, const uint8_t *input, size_t len)
+{
+    fwrite(input, 1, len, (FILE *)env); // XXX loop; use write(2)?
+}
+
+static void print(void *env, const char *fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vfprintf((FILE *)env, fmt, args);
+    va_end(args);
+}
+
+static const char *errorname(DNP3_ParseError e)
+{
+    switch(e) {
+    case ERR_FUNC_NOT_SUPP: return "FUNC_NOT_SUPP";
+    case ERR_OBJ_UNKNOWN:   return "OBJ_UNKNOWN";
+    case ERR_PARAM_ERROR:   return "PARAM_ERROR";
+    }
+    return "???";
+}
+
+
+/// callback functions ///
 
 static void error(void *env, const char *fmt, ...)
 {
@@ -19,24 +46,98 @@ static void error(void *env, const char *fmt, ...)
     va_end(args);
 }
 
-void output_frame(void *env, const DNP3_Frame *frame,
-                  const uint8_t *buf, size_t len);
+void output_ctrl_frame(void *env, const DNP3_Frame *frame,
+                       const uint8_t *buf, size_t len)
+{
+    if(frame->func == DNP3_UNCONFIRMED_USER_DATA ||
+       frame->func == DNP3_CONFIRMED_USER_DATA) {
+        return;
+    }
+
+    output(env, buf, len);
+}
+
 void output_fragment(void *env, const DNP3_Fragment *fragment,
-                     const uint8_t *buf, size_t len);
+                     const uint8_t *buf, size_t len)
+{
+    output(env, buf, len);
+}
+
+void print_frame(void *env, const DNP3_Frame *frame,
+                const uint8_t *buf, size_t len)
+{
+    print(env, "L> %s\n", dnp3_format_frame(frame));
+}
+
+void print_fragment(void *env, const DNP3_Fragment *fragment,
+                     const uint8_t *buf, size_t len)
+{
+    print(env, "A> %s\n", dnp3_format_fragment(fragment));
+}
+
+void print_segment(void *env, const DNP3_Segment *segment)
+{
+    print(env, "T> %s\n", dnp3_format_segment(segment));
+}
+
+void print_transport_payload(void *env, const uint8_t *s, size_t n)
+{
+    print(env, "T: reassembled payload:");
+    for(size_t i=0; i<n; i++)
+        print(env, " %.2X", (unsigned int)s[i]);
+    print(env, "\n");
+}
+
+void print_app_invalid(void *env, DNP3_ParseError e)
+{
+    if(!e)
+        print(env, "A: no parse\n");
+    else
+        print(env, "A: error %s (%d)\n", errorname(e), (int)e);
+}
 
 
 /// main ///
 
+const char *usage =
+    "usage: dissect [-f]\n"
+    "    -f  filter: pass valid traffic to stdout\n"
+    ;
+    
 int main(int argc, char *argv[])
 {
     StreamProcessor *p;
     DNP3_Callbacks callbacks = {NULL};
 
-    dnp3_init();
-
-    callbacks.link_frame = output_frame;
-    callbacks.app_fragment = output_fragment;
+    // default: print mode
+    callbacks.link_frame = print_frame;
+    callbacks.transport_segment = print_segment;
+    callbacks.transport_payload = print_transport_payload;
+    callbacks.app_invalid = print_app_invalid;
+    callbacks.app_fragment = print_fragment;
     callbacks.log_error = error;
+
+    // command line
+    int ch;
+    while((ch = getopt(argc, argv, "hf")) != -1) {
+        switch(ch) {
+        case 'f':
+            // filter mode
+            callbacks.link_frame = output_ctrl_frame;
+            callbacks.transport_segment = NULL;
+            callbacks.transport_payload = NULL;
+            callbacks.app_invalid = NULL;
+            callbacks.app_fragment = output_fragment;
+            break;
+        default:
+            fputs(usage, stderr);
+            return 1;
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    dnp3_init();
 
     p = dnp3_dissector(callbacks, stdout);
     if(p == NULL) {
@@ -67,6 +168,5 @@ int main(int argc, char *argv[])
     }
 
     p->finish(p);
-
     return 0;
 }
